b7990a233b07059789377ab1c28b188048477e0c
charlenetan <tanwsc@gmail.com>
2021-08-16 14:06:47 +0800

    changing token duration

--- lepakdjango/settings.py
+++ lepakdjango/settings.py
@@ -66,7 +66,7 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
