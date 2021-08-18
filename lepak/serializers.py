ef5caa850278810d1cde7f38842216e925a66692
charlenetan <tanwsc@gmail.com>
2021-08-18 17:30:05 +0800

    trying validation in serialiser

--- lepak/serializers.py
+++ lepak/serializers.py
@@ -1,7 +1,11 @@
 from .models import Profile, Journal
+from django.contrib.auth.models import update_last_login
 # from django.contrib.auth.models import User, Group
-from rest_framework import serializers
+from rest_framework import serializers, status
+from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.settings import api_settings
+
 
 
 # ========== Model Serialisers ==========
@@ -20,6 +24,26 @@ class TokenSerializer(serializers.Serializer):
     token = serializers.CharField(max_length=255)
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        if not Profile.objects.get(username=attrs.username):
+            return Response(
+                data={
+                    "message": "User does not exist."
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        refresh = self.get_token(self.user)
+
+        data['refresh'] = str(refresh)
+        data['access'] = str(refresh.access_token)
+
+        if api_settings.UPDATE_LAST_LOGIN:
+            update_last_login(None, self.user)
+
+        return data
+
     def get_token(cls, user):
         token = super().get_token(user)
 
